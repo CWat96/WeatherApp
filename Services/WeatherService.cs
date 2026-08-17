74703b2963f2532d20760880d99bfe773418c541
Waters, Cesily <96clondon@proton.me>
2026-08-16 21:02:17 -0400

    Add WeatherService

---
+++ Services/WeatherService.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherApp.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
