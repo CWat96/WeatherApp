b5660eef140c4a2cb58f098d2549400436e4a514
Waters, Cesily <96clondon@proton.me>
2026-08-16 21:27:40 -0400

    Connecting weather service to weather.razor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using WeatherApp.Components;
+using WeatherApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<WeatherService> ();
 
 var app = builder.Build();
 
