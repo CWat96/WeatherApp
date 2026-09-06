81766112431659abac6e3b90d7561c4c7f86e310
Waters, Cesily <96clondon@proton.me>
2026-09-06 10:29:17 -0400

    Added WeatherService and DI

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -3,10 +3,12 @@
     public class WeatherService
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public WeatherService(HttpClient httpClient)
+        public WeatherService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
     }
 }
