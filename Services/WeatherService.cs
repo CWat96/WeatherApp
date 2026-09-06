fc95b29d9ea5cc50a92b50e66c91da66ad3b1238
Waters, Cesily <96clondon@proton.me>
2026-09-06 10:51:25 -0400

    Inserted async method. No blocking while waiting.

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -4,11 +4,19 @@
     {
         private readonly HttpClient _httpClient;
         private readonly IConfiguration _configuration;
+        private readonly string? _apiKey;
 
         public WeatherService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
             _configuration = configuration;
+
+            _apiKey = _configuration["OpenWeatherMap:ApiKey"];
+        }
+
+        public async Task<string> GetWeatherAsync(string city)
+        {
+
         }
     }
 }
